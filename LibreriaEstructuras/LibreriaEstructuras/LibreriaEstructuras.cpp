9391ea42df4fce7609ff215c08d77e2e2924fb0d
Mark Chandar <PhyXelgato@gmail.com>
2019-10-26 18:50:55 -0600

    Commit 1

--- LibreriaEstructuras/LibreriaEstructuras/LibreriaEstructuras.cpp
+++ LibreriaEstructuras/LibreriaEstructuras/LibreriaEstructuras.cpp
@@ -1,20 +1,53 @@
-// LibreriaEstructuras.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 
+using namespace std;
+
+#pragma region Declaracion de estructuras
+struct nodoPersona
+{
+	int cedula;
+	string nombre;
+	string ap1;
+	string ap2;
+	double balance;
+	string usuario;
+	string password;
+	nodoPersona* siguiente;
+};
+typedef struct nodoPersona* persona;
+
+
+struct nodoLibro
+{
+	string titulo;
+	string autor;
+	int ventas;
+	double rating;
+	string genero;
+	string sinopsis;
+};
+
+struct nodoGlosario {
+
+	char letra;
+	int codigo;
+	nodoGlosario* siguiente;
+	nodoLibro* libro;
+};
+typedef struct nodoGlosario* glosario;
+
+
+
+#pragma endregion
+
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	persona listaClientes;
+	persona listaAdministradores;
+	glosario inventario;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
