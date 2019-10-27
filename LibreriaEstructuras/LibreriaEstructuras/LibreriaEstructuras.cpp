384a5beafb6a622a0b52e80f1b3b153670e968e4
Mark Chandar <PhyXelgato@gmail.com>
2019-10-26 22:14:55 -0600

    Iniciar Glosario

--- LibreriaEstructuras/LibreriaEstructuras/LibreriaEstructuras.cpp
+++ LibreriaEstructuras/LibreriaEstructuras/LibreriaEstructuras.cpp
@@ -38,6 +38,46 @@ struct nodoGlosario {
 typedef struct nodoGlosario* glosario;
 
 
+nodoGlosario* crearLetra(char letra,int codigo) {
+	nodoGlosario* aux = new(struct nodoGlosario);
+	aux->letra = letra;
+	aux->codigo = codigo;
+	aux->siguiente = NULL;
+	aux->codigo = NULL;
+	return aux;
+	
+}
+
+#pragma endregion
+
+
+#pragma region Funciones y otras cosas divertidas
+
+void crearLetra(glosario &cab, char letra,int codigo) {
+	nodoGlosario* nuevo;
+	glosario aux;
+	nuevo = crearLetra(letra, codigo);
+
+	if (cab == NULL)
+	{
+		cab == nuevo;
+		cout << "-> !// Info: se ingreso la letra: " << letra << " con el codigo: "<< codigo<< " //!" << endl;
+	}
+	else
+	{
+	 aux=cab;
+
+	 while (aux->siguiente != NULL) 
+	 aux = aux->siguiente;
+	 aux->siguiente = nuevo;
+	 cout << "-> !// Info: se ingreso la letra: " << letra << " con el codigo: " << codigo << " //!" << endl;
+
+	 
+		
+	}
+
+}
+
 
 #pragma endregion
 
@@ -45,9 +85,41 @@ typedef struct nodoGlosario* glosario;
 
 int main()
 {
-	persona listaClientes;
-	persona listaAdministradores;
-	glosario inventario;
+	cout << "-> !// " << endl;
+	persona listaClientes=NULL;
+	cout << "-> !// Info: listaClientes creado //! " << endl;
+	glosario inventario=NULL;
+	cout << "-> !// Info: inventario creado //! " << endl;
+	system("PAUSE");
+	cout << "-> !// Creando Glosario..." << endl;
+		crearLetra(inventario, 'A', 1);
+		crearLetra(inventario, 'B', 2);
+		crearLetra(inventario, 'C', 3);
+		crearLetra(inventario, 'D', 4);
+		crearLetra(inventario, 'E', 5);
+		crearLetra(inventario, 'F', 6);
+		crearLetra(inventario, 'G', 7);
+		crearLetra(inventario, 'H', 8);
+		crearLetra(inventario, 'I', 9);
+		crearLetra(inventario, 'J', 10);
+		crearLetra(inventario, 'K', 11);
+		crearLetra(inventario, 'L', 12);
+		crearLetra(inventario, 'M', 13);
+		crearLetra(inventario, 'N', 14);
+		crearLetra(inventario, 'O', 15);
+		crearLetra(inventario, 'P', 16);
+		crearLetra(inventario, 'Q', 17);
+		crearLetra(inventario, 'R', 18);
+		crearLetra(inventario, 'S', 19);
+		crearLetra(inventario, 'T', 20);
+		crearLetra(inventario, 'U', 21);
+		crearLetra(inventario, 'V', 22);
+		crearLetra(inventario, 'W', 23);
+		crearLetra(inventario, 'X', 24);
+		crearLetra(inventario, 'Y', 25);
+		crearLetra(inventario, 'Z', 26);
+	
+	
 }
 
 
